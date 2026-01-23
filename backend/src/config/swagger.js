b1c6b8f6cfee08fa298082421b14053aa3398fb0
Githubshakil <shakilmahmud007@gmail.com>
2026-01-24 00:59:03 +0600

    part-7
    swagger config

---
+++ backend/src/config/swagger.js
@@ -0,0 +1,43 @@
+const { version } = require('mongoose')
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+    defination: {
+        openapi: '3.0.0',
+        info: {
+            title : 'Multivendor E-commerce API',
+            version: '1.0.0',
+            description: 'API for large scale multivendor ecommerce (MERN Stack)',
+            contact : {
+                fullname: 'Md Shakil Mahmud',
+                email: 'shakilmahmud007@gmail.com',
+            },
+        },
+
+        servers: [
+            {
+                url: `http://localhost:${process.env.PORT || 5000}`,
+                description: 'Development server'
+            },
+            // {
+            //     url: `http://localhost:${process.env.PORT || 5000}`,
+            //     description: 'Live server '
+            // }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerTokenFormet:'JWT'
+                }
+            }
+        }
+    },
+
+    apis:['./src/routes/*.js', './src/controllers/*.js']
+}
+
+const specs = swaggerJsdoc(options)
+
+module.exports = specs
